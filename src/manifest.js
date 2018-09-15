caa1a24715cdef6b989551c6a5d229c5c70f4328
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-09-15 16:35:26 +0200

    Firefox manifest.json

--- src/manifest.js
+++ src/manifest.js
@@ -1,7 +1,14 @@
 module.exports = (browser, version) => ({
-  "key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhUUr0Z4y8DxD5WIezuYeGW3zDZkvGXST+uviv4jtXwkNVZI5XTUrs/pfYoYsOKdnrxkE47/mI+TiumUj7buB1pC+qeWYjVEshDD5Wum6J44RhXmAo7eb1e3u+IG0BmFvQOO+ENtRxIacZ/M8gexGRIlVWJKuRtcLREc7EkwxtWN58fPrWcYsuXfO3NDEkvrSz7hCCNgeVf/y0MKIz7ZRje8Afb1cRa2PtVAFJ2KUFikzghNFyAY8DVmXXZzrvUDbkeKxA/ja2+5TePhVhKBE+DpFGNUBhb1bEi+pl7ysssxFxjdwiW5/9q58JJeh1Mcu1lnoGIVQTspcd5fbioMNNQIDAQAB",
+  "key": (browser === 'chrome')
+    ? "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhUUr0Z4y8DxD5WIezuYeGW3zDZkvGXST+uviv4jtXwkNVZI5XTUrs/pfYoYsOKdnrxkE47/mI+TiumUj7buB1pC+qeWYjVEshDD5Wum6J44RhXmAo7eb1e3u+IG0BmFvQOO+ENtRxIacZ/M8gexGRIlVWJKuRtcLREc7EkwxtWN58fPrWcYsuXfO3NDEkvrSz7hCCNgeVf/y0MKIz7ZRje8Afb1cRa2PtVAFJ2KUFikzghNFyAY8DVmXXZzrvUDbkeKxA/ja2+5TePhVhKBE+DpFGNUBhb1bEi+pl7ysssxFxjdwiW5/9q58JJeh1Mcu1lnoGIVQTspcd5fbioMNNQIDAQAB"
+    : undefined,
   "manifest_version": 2,
-  "minimum_chrome_version": "34",
+  minimum_chrome_version: (browser === 'chrome') ? "34" : undefined,
+  applications: (browser === 'firefox') ? {
+    "gecko": {
+      "strict_min_version": "50.0"
+    }
+  } : undefined,
   "name": "Code Medium",
   version,
   "description": "Extends the Medium writer interface to quickly create and edit Github Gists, without having to leave the editor.",
@@ -13,7 +20,9 @@ module.exports = (browser, version) => ({
   "short_name": "code-medium",
   "background": {
     "page": "background/background.html",
-    "persistent": true
+    "persistent": (browser === 'firefox')
+      ? undefined // firefox only supports persistent and complains when it's set explicitly
+      : true,
   },
   "browser_action": {
     "default_icon": {
